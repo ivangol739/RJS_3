0131650900c6834f425a2d77b4d2e0f2e93556a1
ivangol739 <ivangol739@gmail.com>
2022-12-18 17:06:35 +0300

    eslint

--- src/js/createField.js
+++ src/js/createField.js
@@ -1,10 +1,8 @@
 export default function createField() {
-  const field = document.querySelector('.field');
-  const n = 4;
-
-  for (let i = 0; i < 4 ** 2; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    field.appendChild(cell)
+  const field = document.querySelector(".field");
+  for (let i = 0; i < 16; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    field.appendChild(cell);
   }
 }
